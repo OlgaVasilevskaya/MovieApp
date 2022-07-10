aeef4b0d172e66db7a3afcbbd62678edc37816ea
Olga Vasilevskaya <oli_vasya@mail.ru>
2022-07-10 19:07:51 +0300

    fix city request

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -16,16 +16,12 @@ const Movies = () => {
 
   useEffect(() => {
     fetchMovies();
-  }, [city]);
+  }, []);
 
-  const handleChange = (e)  => { //не могу понять, почему не срабатывает условие. Подскажи, пжл, что не так
-    if (e.target.city === 'minsk') {
-
-      return setCity(1);
-    } else {
-
-      return setCity(5);
-    }
+  const handleChange = (e) => {
+    e.target.value === 'minsk'
+      ? setCity(1)
+        : setCity(5)
   };
 
   const fetchMovies = useCallback(async () => {
@@ -35,8 +31,6 @@ const Movies = () => {
       const data = await fetch(
         `https://soft.silverscreen.by:8443/wssite/webapi/event/data?filter=%7B%22city%22:${city}%7D&extended=true`
       )
-
-      console.log(city, 'city');
 
       const movieDetails = await data.json();
 
@@ -62,10 +56,11 @@ const Movies = () => {
           ? <Spinner />
           : (
               <>
-                <select className='movies-city' value={city} onChange={handleChange}>
-                  <option value="minsk">minsk</option>
-                  <option value="grodno">grodno</option>
+                <select className='movies-city' onChange={handleChange}>
+                  <option>minsk</option>
+                  <option>grodno</option>
                 </select>
+                {console.log(city)}
 
                 {movies.map((m) => <Movie key={m.eventId} movie={m} />)}
               </>
